15d0e0736afcdef68de8c758c155c9e12273ea5a
Solano-tech <202502014791@alunos.estacio.br>
2025-08-19 04:05:31 +0000

    Conclusao do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,36 +11,35 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
     //Definição das variáveis da carta A
-    char codigoA[3];
-    char cidadeA[10];
+    char codA[5];
+    char cidadeA[20];
     int populacaoA;
+    int ptsTurismoA;
     float areaA;
     float pibA;
-    int ptsTurismoA;
 
-    /*
     //Definição das variáveis da carta B
-    char codigoB[3];
+    char codB[5];
     char cidadeB[20];
     int populacaoB;
+    int ptsTurismoB;
     float areaB;
     float pibB;
-    int ptsTurismoB;
-    */
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
     //Cadastro da carta A
-    printf("Cadastro da Carta A \n");
+    printf("Cadastro da Carta A\n");
 
-    printf("Digite o código da cidade: ");
-    scanf("%s", codigoA); //Leitura do código informado e armazenamento do valor na variável codigoA
-
-    printf("Digite o nome da cidade: ");
+    printf("Informe o nome da cidade: ");
     scanf("%s", cidadeA); //Leitura do nome informado e armazenamento do valor na variável cidadeA
-/*    printf("Informe a área da cidade: ");
+
+    printf("Informe o código da cidade: ");
+    scanf("%s", codA); //Leitura do código informado e armazenamento do valor na variável codA
+
+    printf("Informe a área da cidade: ");
     scanf("%f", &areaA); //Leitura da área da cidade informada e armazenamento do valor na variável areaA
 
     printf("Informe a população da cidade: ");
@@ -50,42 +49,50 @@ int main() {
     scanf("%f", &pibA); //Leitura do PIB da cidade informada e armazenamento do valor na variável pibA
 
     printf("Informe o número de pontos turísticos da cidade: ");
-    scanf("%d", &ptsTurismoA);   //Leitura do número de pontos turísticos da cidade informados e armazenamento do valor na variável ptsTurismoA  
-*/
-/*    //Cadastro da carta B
+    scanf("%d", &ptsTurismoA);   //Leitura do número de pontos turísticos da cidade informados e armazenamento do valor na variável ptsTurismoA     
+
+    //Cadastro da carta B
     printf("Cadastro da Carta B\n");
 
-    printf("Digite o nome da cidade: ");
-    scanf("%s\n", cidadeB); //Leitura do nome informado e armazenamento do valor na variável cidadeB
+    printf("Informe o nome da cidade: ");
+    scanf("%s", cidadeB); //Leitura do nome informado e armazenamento do valor na variável cidadeB
 
-    printf("Digite o código da cidade: ");
-    scanf("%s\n", codigoB); //Leitura do código informado e armazenamento do valor na variável codigoB
+    printf("Informe o código da cidade: ");
+    scanf("%s", codB); //Leitura do código informado e armazenamento do valor na variável codB
 
     printf("Informe a área da cidade: ");
-    scanf("%f\n", &areaB); //Leitura da área da cidade informada e armazenamento do valor na variável areaB
+    scanf("%f", &areaB); //Leitura da área da cidade informada e armazenamento do valor na variável areaB
 
     printf("Informe a população da cidade: ");
-    scanf("%d\n", &populacaoB); //Leitura da população da cidade informada e armazenamento do valor na variável populacaoB
+    scanf("%d", &populacaoB); //Leitura da população da cidade informada e armazenamento do valor na variável populacaoB
 
     printf("Informe o PIB da cidade: ");
-    scanf("%f\n", &pibB); //Leitura do PIB da cidade informada e armazenamento do valor na variável pibB
+    scanf("%f", &pibB); //Leitura do PIB da cidade informada e armazenamento do valor na variável pibB
 
     printf("Informe o número de pontos turísticos da cidade: ");
-    scanf("%d\n", &ptsTurismoB);   //Leitura do número de pontos turísticos da cidade informados e armazenamento do valor na variável ptsTurismoB     
-*/
+    scanf("%d", &ptsTurismoB);   //Leitura do número de pontos turísticos da cidade informados e armazenamento do valor na variável ptsTurismoB  
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     //Exibição da carta A
     printf("\nCARTA A\n");
-    printf("Código: %s\n", codigoA);
     printf("Nome da Cidade: %s\n", cidadeA);
-
-/*    printf("Área: %.1f km2 \n", areaA);
+    printf("Código: %s\n", codA);
+    printf("Área: %.1f km2 \n", areaA);
     printf("População: %d \n", populacaoA);
-    printf("PIB: R$ %.1f \n", pibA);
-    printf("Número de pontos Turísticos: %d \n", ptsTurismoA);
-*/
+    printf("PIB: R$ %.1f bilhões \n", pibA);
+    printf("Número de Pontos Turísticos: %d \n", ptsTurismoA);
+
+    //Exibição da carta B
+    printf("\nCARTA B\n");
+    printf("Nome da Cidade: %s\n", cidadeB);
+    printf("Código: %s\n", codB);
+    printf("Área: %.1f km2 \n", areaB);
+    printf("População: %d \n", populacaoB);
+    printf("PIB: R$ %.1f bilhões \n", pibB);
+    printf("Número de Pontos Turísticos: %d \n", ptsTurismoB);
+
     return 0;
 }
